3ce9b4f6cbf3a05c7ebd0eee5d78fbff38e85ffa
Meregali-bat <lucas.smeregalli@gmail.com>
2025-04-08 00:37:30 -0300

    add-getuser.route

--- src/v1/user/index.js
+++ src/v1/user/index.js
@@ -3,7 +3,8 @@ const router = require('express').Router();
 const signUp = require('./signup.route');
 const editUser = require('./edituser.route');
 const authUser = require('./authuser.route');
+const getUser = require('./getuser.route');
 
-router.use(signUp, editUser, authUser);
+router.use(signUp, editUser, authUser, getUser);
 
 module.exports = router;
